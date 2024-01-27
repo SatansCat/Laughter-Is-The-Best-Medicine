193ce4c31bf07a5cbdfc41e7360f7ae9e15537a2
TonyMHepburn <TonyMHepburn@gmail.com>
2024-01-27 02:45:19 +0000

    Reimplementation of File organisation

--- Laughter Is The Best Medicine/Assets/Source/Player_Controller.cs
+++ Laughter Is The Best Medicine/Assets/Source/Player_Controller.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Player_Movement : MonoBehaviour
+public class Player_Controller: MonoBehaviour
 {
-    public float speed = .5f;
-
     Rigidbody2D rBody2D;
 
+    //Variables for movement
+    public float speed = .5f;
     Vector2 movement;
+
+    //public variables for interaction logic
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
 
     void Start()
     {

8ad38860a69164ef2c603558d368f8ce43d5ab0d
Emma Neeson <emmaneesoneire@gmail.com>
2024-01-28 16:46:01 +0000

    Start Menu Scene Change

--- Laughter Is The Best Medicine/Assets/Source/UI.cs
+++ Laughter Is The Best Medicine/Assets/Source/UI.cs
@@ -3,8 +3,18 @@ using UnityEngine.UIElements;
 
 public class UI : MonoBehaviour
 {
+    public LevelLoad loadGame;
+
    private void OnEnable()
     {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
 
+        Button buttonStart = root.Q<Button>("Start_Button");
+        // Button buttonSetting = root.Q<Button>("Settings_Button");
+        Button buttonExit = root.Q<Button>("Exit_Button");
+
+        buttonStart.clicked += () => loadGame.LoadLevel();
+        // buttonSetting.clicked += () =>
+        buttonExit.clicked += () => Application.Quit();
     }
 }

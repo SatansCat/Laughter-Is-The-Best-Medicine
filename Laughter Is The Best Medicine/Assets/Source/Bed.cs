e6f37a3b391ea36496c244afefb2578e6a708dcf
TonyMHepburn <TonyMHepburn@gmail.com>
2024-01-28 07:07:44 +0000

    ReadSpeaker.AI will now read out the joke when it's delivered to the bed!
    ENDA. ENDA WHY. ENDA WHY DID YOU NOT FIX THE GITIGNORE. MY POOR FUCKING WRIST. SO MANY CLICKS. SO MANY CLIHIHIHIIIIIICCCCKKKKKSSSSS
    
    ReadSpeaker.AI Plugin installed, all files pushed to git.
    Bed now contains public TTSSpeaker variable called speaker.  This is initialised in Start. In the interaction section, before clearing the joke data, the speaker is now called and reads the joke.
    
    Speaker gameobject added to scene.  This has a TTS Speaker component + AudioSource component (referenced by the TTS Speaker). Bed prefab needs to have this Speaker GameObject assigned to its Speaker variable.

--- Laughter Is The Best Medicine/Assets/Source/Bed.cs
+++ Laughter Is The Best Medicine/Assets/Source/Bed.cs
@@ -1,3 +1,4 @@
+using ReadSpeaker;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,7 @@ public class Bed : MonoBehaviour
     private Patient m_bedPatient;
     [SerializeField] private GameObject timer;
     [SerializeField] private bool m_occupied;
+    public TTSSpeaker speaker;
     //disease icon
 
     public Patient BedPatient
@@ -27,14 +29,22 @@ public class Bed : MonoBehaviour
     void Start()
     {
         BedManager.instance.RegisterBed(this);
+        TTS.Init();
     }
 
     public void Interaction()
     {
         if (GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Movement>().HasJoke == true)
         {
+            //Read the joke out loud
+            //Debug.Log(GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Movement>().TheJoke);
+            TTS.Say(GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Movement>().TheJoke, speaker);
+            //Remove joke from player
             GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Movement>().HasJoke = false;
             GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Movement>().TheJoke = null;
+            //Make sure no new lines start playing until the current is done
+            //Below line will not exit until the joke is finished, this was in the tutorial but I don't think we would need it.
+            //yield return new WaitUntil(() => !speaker.audioSource.isPlaying);
         }
         else
         {
